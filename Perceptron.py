9ee407cd3a9bc36480899fd8c6c3f213390d3efd
Edward Wong <edward4wong@gmail.com>
2015-05-12 01:55:24 -0700

    convert labels to -1,1

--- Perceptron.py
+++ Perceptron.py
@@ -18,12 +18,22 @@ def load(fn, ds):
 		data = (features, int(tokens[len(tokens) - 1]))
 		ds.append(data)
 
+def convertLabel(ds, label_neg, label_pos):
+	for i in range(0, len(ds)):
+		(features, label) = ds[i]
+		if label == label_pos:
+			ds[i] = (features, 1)
+		elif label == label_neg:
+			ds[i] = (features, -1)
+		else:
+			print "uh oh"
+
 load("hw4atrain.txt", training_set_a)
 load("hw4atest.txt",test_set_a)
 load("hw4btrain.txt", training_set_b)
 load("hw4btest.txt", test_set_b)
 
-print len(training_set_a)
-print len(test_set_a)
-print len(training_set_b)
-print len(test_set_b)
+convertLabel(training_set_a, 0, 6)
+convertLabel(test_set_a, 0 ,6)
+
+
