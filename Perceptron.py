370f341be64acd460d509c7981fe965dcf8c95d9
Benjamin Genchel <benjiegenchel@gmail.com>
2015-05-13 00:25:19 -0700

    successful testing of numpy integration

--- Perceptron.py
+++ Perceptron.py
@@ -5,6 +5,7 @@ import random
 import copy
 
 def load(fn):
+	print "loading " + fn + "..."
 	data_matrix = []
 	f = open(fn, "r")
 	for line in f:
@@ -18,7 +19,7 @@ def load(fn):
 
 
 def convertLabel(data_matrix, label_neg, label_pos):
-	for i in range(len(ds)):
+	for i in range(len(data_matrix)):
 		(features, label) = data_matrix[i]
 		if label in label_pos:
 			data_matrix[i] = (features, 1)
@@ -26,31 +27,6 @@ def convertLabel(data_matrix, label_neg, label_pos):
 			data_matrix[i] = (features, -1)
 		else:
 			print "uh oh"
-
-
-# def init_w(sample):
-# 	w = []
-# 	for i in range(0, len(sample[0])):
-# 		w.append(0)
-# 	return np.array(w)
-
-
-# def dot_product(a, b):
-# 	res = 0
-# 	for i in range(0, len(a)):
-# 		res = res + a[i] * b[i]
-# 	return res
-
-# def add_vector(vector0, vector1, y):
-# 	res = []
-# 	for i in range(0, len(vector0)):
-# 		if y == 1:
-# 			res.append(vector0[i] + vector1[i])
-# 		elif y == -1:
-# 			res.append(vector0[i] - vector1[i])
-# 		else:
-# 			print "uhoh"
-# 	return res
 
 
 #i don't understand what is being appended to res here;
@@ -64,7 +40,7 @@ def perceptron(training_set):
 		if label*np.dot(w, features) <= 0:
 			res.append((w, counter))
 			counter = 1
-			w = add_vector(w, label*features)
+			w = w + label*features
 		else:
 			counter = counter + 1
 	res.append((w, counter))
@@ -93,11 +69,6 @@ def voted_perceptron(test_set, results):
 			errors = errors + 1
 	return float(errors)/float(num_samples)
 
-# def scale(original, scalar):
-# 	scaled = []
-# 	for i in range(len(original)):
-# 		scaled.append(original[i] * scalar)
-# 	return scaled
 
 def average_scaled_w(results):
 	average_w = results[0][0]
@@ -166,6 +137,7 @@ def main():
 	training_set_b = load("hw4btrain.txt")
 	test_set_b = load("hw4btest.txt")
 	debug_set = load("test.txt")
+	print "\n"
 
 	convertLabel(training_set_a, [0], [6])
 	convertLabel(test_set_a, [0], [6])
